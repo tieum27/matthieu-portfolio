9decb70cab091ac9a3ce1c6880a6403c1dd2341b
tieum27 <tieum27@hotmail.com>
2017-11-06 17:21:03 -0800

    prep for deploy

--- web/Amber/calc/calc1/index.php
+++
@@ -1,9 +0,0 @@
-<head>
-<title>Mortgage Calculator</title>
-<link href="calculator.css" type="text/css" rel="stylesheet">
-</head>
-
-<body>
-<?php include("FreeMortgageCalculatorWithCharts.php"); ?>
-</body>
-</html>
